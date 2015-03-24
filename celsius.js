39fc4a4b503ff2d7c8d02ea06737dfb47570715d
andreadelrio <delrio.andre@gmail.com>
2015-03-23 22:54:06 -0500

    celsius

---
+++ celsius.js
@@ -0,0 +1,4 @@
+var fahrenheit = prompt("Ingresa temperatura en grados Fahrenheit");
+var fahrenheitNum = parseInt(fahrenheit);
+var celsius = (fahrenheitNum - 32) * 5/9;
+alert("La temperatura en grados Celsius es: " + celsius);
--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
 // 1.
-alert("Hola desde Laboratoria!");
+alert("Esta alerta viene del archivo mi-script.js");
 // 2.
 document.write("<h1>Bienvenidas a la intro a Javascript!</h1>");
